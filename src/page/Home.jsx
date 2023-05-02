fb3255a2088f2d27f056610bb5dbdbdd13d078c1
Kiisi <kiisifelix06@gmail.com>
2023-05-02 23:03:00 +0100

    Feat - Solutions Section In home page'

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../layout/Navbar'
 import Footer from '../layout/Footer'
 import heroImg from '../assets/hero-img.svg'
+import dropdown_icon from '../assets/Down Button.svg'
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Navbar />
             <Hero />
             <ComingSoon />
+            <Solutions />
             <Footer />
         </>
     )
@@ -37,17 +39,17 @@ const Hero = () => {
 
 const ComingSoon = () => {
 
-    const data = ["You are a female living in the MENA region", "You want to create more fun & financial freedom in your life","You have an interest in Web3, Metaverse, Blockchain, IOT, Machine Learning, AI and other emerging tech","You are looking to upskill and stay ahead of the curve","You are an investor looking to expand your knowledge and investment opportunities in this area","You are ready to disrupt the emerging tech industry"]
-    
-    
+    const data = ["You are a female living in the MENA region", "You want to create more fun & financial freedom in your life", "You have an interest in Web3, Metaverse, Blockchain, IOT, Machine Learning, AI and other emerging tech", "You are looking to upskill and stay ahead of the curve", "You are an investor looking to expand your knowledge and investment opportunities in this area", "You are ready to disrupt the emerging tech industry"]
+
+
     return (
         <section className="px-6">
             <div className="coming-soon-bg mx-auto w-full max-w-[40rem] rounded-xl">
-                <div className="px-10 py-16 backdrop-blur-[2px] max-w-[35rem] mx-auto">
+                <div className="px-10 py-16 backdrop-blur-[8px] max-w-[35rem] mx-auto">
                     <header className="text-center text-white">
-                    <h1 className="text-[40px] text-white font-bold">Coming Soon!</h1>
-                    <h2 className="font-semibold text-[22px]">Sign Up To Be Notified When We Launch</h2>
-                    <p className="text-[19px]">You may be interested in our launch if:</p>
+                        <h1 className="text-[40px] text-white font-bold">Coming Soon!</h1>
+                        <h2 className="font-semibold text-[22px]">Sign Up To Be Notified When We Launch</h2>
+                        <p className="text-[19px]">You may be interested in our launch if:</p>
                     </header>
 
                     <div className="text-white pt-5">
@@ -59,8 +61,8 @@ const ComingSoon = () => {
                     </div>
 
                     <div className="flex flex-col gap-y-6 mt-10">
-                        <input className="py-2 rounded-md border-2 outline-0 border-[#D8BB78] bg-[#ffffff4d] text-white text-center" placeholder="Your First Name"/>
-                        <input className="py-2 rounded-md border-2 outline-0 border-[#D8BB78] bg-[#ffffff4d] text-white text-center" type="email" placeholder="Your e-mail"/>
+                        <input className="py-2 rounded-md border-2 outline-0 border-[#D8BB78] bg-[#ffffff4d] text-white text-center" placeholder="Your First Name" />
+                        <input className="py-2 rounded-md border-2 outline-0 border-[#D8BB78] bg-[#ffffff4d] text-white text-center" type="email" placeholder="Your e-mail" />
                         <button className="bg-[#D8BB78] rounded-md text-white font-semibold py-3">Join</button>
                     </div>
                 </div>
@@ -68,4 +70,53 @@ const ComingSoon = () => {
             </div>
         </section>
     )
+}
+
+// Solutions
+
+const Solutions = () => {
+
+    return (
+        <section className="py-[5rem] px-6 max-w-[1500px] mx-auto">
+            <header className="max-w-[60rem]">
+                <h1 className="bg-primary text-[32px] font-bold text-white pl-1 pr-5 max-w-fit">Solutions</h1>
+                <p className="mt-5 text-[#1A1A1A] text-[22px]">Exsel will create an inclusive environment for women to pursue knowledge and
+                    careers in emerging technologies. The platform will offer a safe and supportive
+                    community for learners to connect and collaborate.
+                </p>
+            </header>
+            <div className="mt-10">
+                <div className="flex justify-between items-center border-b-[1px] border-[#000] py-4">
+                    <h2 className="font-bold text-[24px]">E-learning</h2>
+                    <div>
+                        <img src={dropdown_icon} alt="Dropdown Icon" className="h-[40px]"/>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center border-b-[1px] border-[#000] py-5">
+                    <h2 className="font-bold text-[24px]">Recruitment Pipeline</h2>
+                    <div>
+                        <img src={dropdown_icon} alt="Dropdown Icon" className="h-[40px]"/>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center border-b-[1px] border-[#000] py-5">
+                    <h2 className="font-bold text-[24px]">Incentivization</h2>
+                    <div>
+                        <img src={dropdown_icon} alt="Dropdown Icon" className="h-[40px]"/>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center border-b-[1px] border-[#000] py-5">
+                    <h2 className="font-bold text-[24px]">Exsel Hackathon</h2>
+                    <div>
+                        <img src={dropdown_icon} alt="Dropdown Icon" className="h-[40px]"/>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center border-b-[1px] border-[#000] py-5">
+                    <h2 className="font-bold text-[24px]">Mentorship</h2>
+                    <div>
+                        <img src={dropdown_icon} alt="Dropdown Icon" className="h-[40px]"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
